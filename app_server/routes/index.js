3973874d5e97ab93e545855ffcb6a07c7c26a571
aliuyguralp <ali.ikariam2000gmail.com>
2022-11-22 20:04:04 +0300

    odev5

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,8 +3,11 @@ var router = express.Router();
 var ctrlMekanlar = require('../controllers/mekanlar');
 var ctrlDigerleri = require('../controllers/digerleri');
 
-router.get('/',ctrlMekanlar.anaSayfa);
-router.get('/mekan',ctrlMekanlar.mekanBilgisi);
-router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle);
-router.get('/hakkinda',ctrlDigerleri.hakkinda);
+router.get('/', ctrlMekanlar.anaSayfa );
+router.get('/mekan', ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
+router.get('/hakkinda', ctrlDigerleri.hakkinda);
+
+
+
 module.exports = router;
